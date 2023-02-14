f6c7becf805070585546827c2964115e6352755f
unknown <existence.m.p@gmail.com>
2023-02-15 01:50:38 +0900

    完成後の最初のcommit

--- mstdn_promoter.js
+++ mstdn_promoter.js
@@ -1,23 +1,43 @@
+var hist = ["r1"];
 window.onload = function(){
     let li = document.getElementsByTagName("li");
+    // 最初のカード以外は隠す
+    li[0].classList.add("first");
     for(let i = 0; i < li.length; i++){
+        // カード内の全てのボタンにイベント設定
         let nextButton = li[i].getElementsByClassName("next");
         for(let j = 0; j < nextButton.length; j++){
-            nextButton[j].addEventListener("click", function(){
-                li[i].classList.remove("answer");
-                li[i].classList.add("answered");
-                if(i + 1 < li.length){
-                    li[i + 1].classList.add("answer");
-                    li[i + 1].classList.remove("hide");
+            let bt = nextButton[j];
+            bt.addEventListener("click", function(){
+                li[i].classList.remove("current");   // 現在答え中フラグ解除
+                li[i].classList.remove("current_back");   // 現在答え中フラグ解除
+                li[i].classList.add("answered");    // 答え済みに設定
+                let targetId = bt.getAttribute("next");
+                let target = document.getElementById(targetId);
+                if(target){
+                    target.classList.add("current");
+                    target.classList.remove("hide");
                 }
+                hist.push(targetId);
             });
         }
-        if(i > 0){
-            li[i].classList.add("hide");
-        }
     }
-}
-function count(i){
-    let e = document.getElementById("counter");
-    e.innerText = parseInt(e.innerText) + i;
+    // 戻るボタン
+    document.getElementById("back").onclick = function(){
+        if(hist.length - 2 >= 0){
+            let li = document.getElementsByTagName("li");
+            for(let i = 0; i < li.length; i++){
+                li[i].classList.add("hide");
+                li[i].classList.remove("current");
+                li[i].classList.remove("current_back");
+                li[i].classList.remove("answered");
+                li[i].classList.remove("first");
+            }
+            let target = document.getElementById(hist[hist.length - 2]);
+            target.classList.remove("hide");
+            target.classList.add("current_back");
+            hist.pop();
+        }
+    };
+    
 }
